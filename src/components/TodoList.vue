d0cb353cf2265b9c2d6b307b1210e593b59679de
Andres Oliveira <andresoliveira@protonmail.com>
2024-08-07 11:01:24 -0300

    add: Todo Form

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,11 +1,21 @@
 <template>
-  
+  <div v-for="todo in todoList" :key="todo.id">
+    <div>{{ todo.item }}</div>
+  </div>
 </template>
 
 <script>
+import { useTodoListStore } from '@/store/useTodoListStore';
+import { storeToRefs } from 'pinia';
+
 export default {
   setup() {
-    
+    const store = useTodoListStore()
+    const { todoList } = storeToRefs(store)
+
+    return {
+      todoList
+    }
   }
 }
 </script>
