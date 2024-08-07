92f343dc8a6f3bf1e851e3ab396396bf9854fa9a
Andres Oliveira <andresoliveira@protonmail.com>
2024-08-07 09:24:53 -0300

    add: pinia

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+.use(createPinia())
+.mount('#app')
