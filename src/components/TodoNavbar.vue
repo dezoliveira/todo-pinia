fe7352f70b08a242ffe828bd98655b2c83d2c574
Andres Oliveira <andresoliveira@protonmail.com>
2024-08-09 08:58:19 -0300

    add: todoList countdown notification

--- src/components/TodoNavbar.vue
+++ src/components/TodoNavbar.vue
@@ -4,15 +4,6 @@
     <a class="btn btn-ghost text-xl">Todo-pinia</a>
   </div>
   <div class="navbar-end">
-    <div class="dropdown dropdown-end">
-      <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
-        <div class="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS Navbar component"
-            src="../assets/logo-pinia.png" />
-        </div>
-      </div>
-    </div>
     <button class="btn btn-ghost btn-circle">
       <div class="indicator">
         <svg
@@ -27,16 +18,32 @@
             stroke-width="2"
             d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
         </svg>
-        <span class="badge badge-xs badge-primary indicator-item"></span>
+        <span class="badge badge-xs badge-primary indicator-item">{{ todoList.length }}</span>
       </div>
     </button>
+    <div class="dropdown dropdown-end">
+      <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+        <div class="w-10 rounded-full">
+          <img
+            alt="Tailwind CSS Navbar component"
+            src="../assets/logo-pinia.png" />
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 </template>
   
 <script>
+import { storeToRefs } from 'pinia'
+import { useTodoListStore } from '@/store/useTodoListStore'
 export default {
-  
+  setup() {
+    const store = useTodoListStore()
+    const { todoList } = storeToRefs(store)
+
+    return { todoList }
+  }
 }
 </script>
 
